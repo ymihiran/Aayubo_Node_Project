9dc67a19455e8d42a54c9f010fd6010d8effbbb2
Yasantha Mihiran <yasanthamax@gmail.com>
2022-07-01 17:34:03 +0530

    first commit

---
+++ routes/people.routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const mysqlConnection = require('../connection.js');
+
+router.get('/', (req, res) => {
+    mysqlConnection.query('SELECT * FROM people', (err, rows, fields) => {
+        if (!err)
+            res.send(rows);
+        else
+            console.log(err);
+    })
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+const express = require('express');
+const bodyParser = require('body-parser');
+const mysqlConnection = require('./connection.js');
+const peopleRoute = require('./routes/people.routes');
+
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.use('/people', peopleRoute);
+
+
+app.listen(3000, () => console.log('Express server is runnig at port no : 3000'));
+
+module.exports = mysqlConnection;
